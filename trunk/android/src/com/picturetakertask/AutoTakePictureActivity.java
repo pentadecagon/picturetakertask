d266536874309caa41cc89f3b7da61bb45ff5283
lcrudd@googlemail.com <lcrudd@googlemail.com@896f4a4a-5d54-1ff1-5b0f-4aa14f184cd5>
2013-03-21 19:08:00 +0000

    (i) fixed problem where photo was being saved at the wrong rotation (ii) moved photo processing to an async task because since the change described in (i) it was taking too long

--- trunk/android/src/com/picturetakertask/AutoTakePictureActivity.java
+++ trunk/android/src/com/picturetakertask/AutoTakePictureActivity.java
@@ -12,10 +12,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.hardware.Camera.AutoFocusCallback;
 import android.hardware.Camera.Parameters;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -24,9 +26,7 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 /**
  * This class simply takes one picture and then finishes.
@@ -51,6 +51,9 @@ public class AutoTakePictureActivity extends Activity implements SurfaceHolder.C
       
       //name of album in gallery containing pictures produced by this activity
       public static final String ALBUM_NAME = "PictureTakerTask";
+      
+      //picture needs to be rotated before it is stored, otherwise it is stored at the wrong angle
+      private float amountToRotatePictureBy;
      
     /** Called when the activity is first created. */
     @Override
@@ -58,6 +61,9 @@ public class AutoTakePictureActivity extends Activity implements SurfaceHolder.C
     {
     	Log.d("camerabasic", "AutoTakePictureActivity.onCreate called");
         super.onCreate(savedInstanceState);
+        
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.auto_take_picture);
 
@@ -118,7 +124,7 @@ public class AutoTakePictureActivity extends Activity implements SurfaceHolder.C
     	  mCamera.setParameters(parameters);
     	  
           mCamera.startPreview();
-          
+
           //set callback for when preview has been initialized
           mCamera.setOneShotPreviewCallback(oneShotPreviewCallback);
       }
@@ -126,7 +132,7 @@ public class AutoTakePictureActivity extends Activity implements SurfaceHolder.C
       /**
        * Set the camera orientation based on the phone rotation
        */
-      public static void setCameraDisplayOrientation(Activity activity,
+      public void setCameraDisplayOrientation(Activity activity,
               int cameraId, android.hardware.Camera camera) {
           android.hardware.Camera.CameraInfo info =
                   new android.hardware.Camera.CameraInfo();
@@ -134,11 +140,23 @@ public class AutoTakePictureActivity extends Activity implements SurfaceHolder.C
           int rotation = activity.getWindowManager().getDefaultDisplay()
                   .getRotation();
           int degrees = 0;
+          amountToRotatePictureBy = 0f;
           switch (rotation) {
-              case Surface.ROTATION_0: degrees = 0; break;
-              case Surface.ROTATION_90: degrees = 90; break;
-              case Surface.ROTATION_180: degrees = 180; break;
-              case Surface.ROTATION_270: degrees = 270; break;
+              case Surface.ROTATION_0:
+            	  amountToRotatePictureBy = 90f;
+            	  degrees = 0;
+            	  break;
+              case Surface.ROTATION_90:
+            	  amountToRotatePictureBy = 0f;
+            	  degrees = 90;
+            	  break;
+              case Surface.ROTATION_180:
+            	  degrees = 180;
+            	  break;
+              case Surface.ROTATION_270:
+            	  amountToRotatePictureBy = 180f;
+            	  degrees = 270;
+            	  break;
           }
 
           int result;
@@ -218,39 +236,60 @@ public class AutoTakePictureActivity extends Activity implements SurfaceHolder.C
         public void onPictureTaken(byte[] data, Camera camera)
         {
        	 	  Log.d("camerabasic", "onPictureTaken called");
+       	 	  new SavePhotoTask().execute(data);
+       	 	finish();
 
-              FileOutputStream outStream = null;
-                   try{
-                   	File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-             	              Environment.DIRECTORY_PICTURES), ALBUM_NAME);
-                   	
-                   	if (! mediaStorageDir.exists()){
-               	        if (! mediaStorageDir.mkdirs()){
-               	            Log.d("camerabasic", "failed to create directory");
-               	            throw new RuntimeException("failed to create directory");
-               	        }
-               	    }
-                   	
-                   	Date lm = new Date(System.currentTimeMillis());
-            		String lasmod = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(lm);
-                   	String fileName = mediaStorageDir.getPath()+"/Image_"+lasmod+".jpg";
-                   	
-                       outStream = new FileOutputStream(fileName);
-                       outStream.write(data);
-                       outStream.close();
-                       
-                       File mediaFile = new File(fileName);
-                       sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.fromFile(mediaFile)));
-                       
-                   } catch (FileNotFoundException e){
-                       Log.d("CAMERA", e.getMessage());
-                   } catch (IOException e){
-                       Log.d("CAMERA", e.getMessage());
-                   }
-
-                   finish();
         }
       };
+      
+      class SavePhotoTask extends AsyncTask<byte[], String, String> {
+    	  @Override
+          protected String doInBackground(byte[]... dataRaw) {
+
+    		  byte[] data = dataRaw[0];
+              FileOutputStream outStream = null;
+              try{
+              	File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+        	              Environment.DIRECTORY_PICTURES), ALBUM_NAME);
+              	
+              	if (! mediaStorageDir.exists()){
+          	        if (! mediaStorageDir.mkdirs()){
+          	            Log.d("camerabasic", "failed to create directory");
+          	            throw new RuntimeException("failed to create directory");
+          	        }
+          	    }
+              	
+               //rotate bitmap by the necessary angle so it's saved upright
+               Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+               int width = bmp.getWidth();
+               int height = bmp.getHeight();
+               Matrix matrix = new Matrix();               
+               matrix.postRotate(amountToRotatePictureBy, width/2.0f, height/2.0f);
+               Bitmap rotatedBmp = Bitmap.createBitmap(bmp, 0, 0,width, height, matrix, true);
+               
+               Date date = new Date(System.currentTimeMillis());
+       		   String datemod = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(date);
+               String fileName = mediaStorageDir.getPath()+"/Image_"+datemod+".jpg";
+              	
+                  outStream = new FileOutputStream(fileName);
+                  //outStream.write(data);
+                  rotatedBmp.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                  outStream.close();
+                  
+                  File mediaFile = new File(fileName);
+                  sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.fromFile(mediaFile)));
+                  
+              } catch (FileNotFoundException e){
+                  Log.d("CAMERA", e.getMessage());
+              } catch (IOException e){
+                  Log.d("CAMERA", e.getMessage());
+              }
+
+              
+    		  return null;
+    	  }
+
+      }
       
       /**
        * Called whenever the surface is created
